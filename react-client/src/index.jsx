cd785c3577bff3c374348b97cd3a94127377ebf2
Hunter Leeves <hunterleeves@gmail.com>
2018-08-30 16:34:20 -0500

    client-server connection established

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -16,29 +16,45 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
+    //this will auto get my albums. Save for later
+    // $.ajax({
+    //   url: '/items', 
+    //   success: (data) => {
+    //     this.setState({
+    //       items: data
+    //     })
+    //   },
+    //   error: (err) => {
+    //     console.log('err', err);
+    //   }
+    // });
   }
 
   handleUserInput(text){
+    console.log('HERE IS THE user INPUT: ',text)
+    
     event.preventDefault()
-    console.log('I FUCKIN PASSED THE FUNCTION DOWN, HERE IS THE TEXT: ',text)
     
     this.setState({
       userInput: text
     })
 
-    axios.get('dicgosAPIhere').then(response => console.log('here is my response: ',response)
-  }
+    $.ajax({
+      url:'/items',
+      method: 'POST',
+      data: {text:text},
+      success: function (returnedData){
+        console.log('I CLICKED AND POST REQUEST TO ITEMS COMPLETE BOIIII')
+        
+        console.log('here is my react returnedData: ',returnedData)
+      },
+      error: function (errorThrown) {
+        console.log('POST REQUEST FROM CLICKING SEARCH FAILED, RIP :',errorThrown)
+      }
+    })
+    
+   }
+  
 
   render () {
     return (<div>
--- server/index.js
+++ server/index.js
@@ -1,26 +1,38 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const items = require('../database-mysql');
 
-var items = require('../database-mysql');
+const app = express();
 
-var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.get('/',function(req,res){
   console.log('yo bros')
-})
+  res.status(200).send('u gucci boi')
+});
 
 app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      console.log('There was an error in the request')
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+  // items.selectAll(function(err, data) {
+  //   if(err) {
+  //     console.log('There was an error in the request')
+  //     res.sendStatus(500);
+  //   } else {
+  //     res.json(data);
+  //   }
+  // });
+ 
+  res.status(200).send('connection established',req)
 });
+
+
+
+app.post('/items',function(req,res){
+  console.log('here is my request body: ',req.body)
+  res.send('post request working')
+});
+
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
