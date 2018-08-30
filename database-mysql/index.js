20e54c09656c328fbad00678d784c4455316abc2
Hunter Leeves <hunterleeves@gmail.com>
2018-08-30 17:04:52 -0500

    dropping them dups

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -8,13 +8,13 @@ var connection = mysql.createConnection({
 });
 
 var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results, fields) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
-    }
-  });
+  // connection.query('SELECT * FROM items', function(err, results, fields) {
+  //   if(err) {
+  //     callback(err, null);
+  //   } else {
+  //     callback(null, results);
+  //   }
+  // });
 };
 
 module.exports.selectAll = selectAll;
